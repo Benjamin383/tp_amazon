f456770f0d84dd4b3e1870f6c0dfd6fe4f80d69a
Benjamin383 <brieucau@gmail.com>
2024-07-04 16:21:27 +0200

    Inscription form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Controller;
 
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -16,11 +26,40 @@ class UserController extends AbstractController
         ]);
     }
 
+    // #[Route('/inscription', name: 'inscription')]
+    // public function inscription(): Response
+    // {
+    //     return $this->render('user/inscription.html.twig', [
+    //         'title' => "Inscription à l'Amazonie",
+    //     ]);
+    // }
+
     #[Route('/inscription', name: 'inscription')]
-    public function inscription(): Response
+    public function inscription(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('user/inscription.html.twig', [
             'title' => "Inscription à l'Amazonie",
+            'inscriptionForm' => $form->createView(),
         ]);
     }
+
 }
