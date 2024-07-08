9e882951571f659126da1fe26f36f6f1b77362f5
Benjamin383 <brieucau@gmail.com>
2024-07-08 09:39:59 +0200

    Crud Commercants

--- src/Controller/CommercantsController.php
+++ src/Controller/CommercantsController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Commercants;
+use App\Form\CommercantsType;
+use App\Repository\CommercantsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/commercants')]
 class CommercantsController extends AbstractController
 {
-    #[Route('/commercants', name: 'app_commercants')]
-    public function index(): Response
+    #[Route('/', name: 'app_commercants_index', methods: ['GET'])]
+    public function index(CommercantsRepository $commercantsRepository): Response
     {
         return $this->render('commercants/index.html.twig', [
-            'controller_name' => 'CommercantsController',
+            'commercants' => $commercantsRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_commercants_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $commercant = new Commercants();
+        $form = $this->createForm(CommercantsType::class, $commercant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($commercant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commercants_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('commercants/new.html.twig', [
+            'commercant' => $commercant,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_commercants_show', methods: ['GET'])]
+    public function show(Commercants $commercant): Response
+    {
+        return $this->render('commercants/show.html.twig', [
+            'commercant' => $commercant,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_commercants_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Commercants $commercant, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CommercantsType::class, $commercant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commercants_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('commercants/edit.html.twig', [
+            'commercant' => $commercant,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_commercants_delete', methods: ['POST'])]
+    public function delete(Request $request, Commercants $commercant, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$commercant->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($commercant);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_commercants_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/CommercantsType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commercants;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommercantsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('id_user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commercants::class,
+        ]);
+    }
+}
